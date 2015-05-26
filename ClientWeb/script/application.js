f7d38d762b6fff819c2c1420b25e135455651780
Dragomir Todorov <dragoto91@gmail.com>
2015-05-27 01:33:24 +0300

    Facebook login init

--- ClientWeb/script/application.js
+++ ClientWeb/script/application.js
@@ -5,6 +5,28 @@ $(document).ready(function()
   var mainC = $('#mainContent'); 
   Parse.initialize("WxrA9CtdMQ1kVF3sZgxtWdqDxsOhJC1bkvr5NyKL", "uRvdUCYFENssbGDeJYsQwAwyoBIIt9Smf4VobpXf");
   
+  
+  window.fbAsyncInit = function() {
+    Parse.FacebookUtils.init({ // this line replaces FB.init({
+      appId      : '844853188931475', // Facebook App ID
+      status     : true,  // check Facebook Login status
+      cookie     : true,  // enable cookies to allow Parse to access the session
+      xfbml      : true,  // initialize Facebook social plugins on the page
+      version    : 'v2.2' // point to the latest Facebook Graph API version
+    });
+
+        // Run code after the Facebook SDK is loaded.
+  };
+
+      (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "http://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  
+  
   // GUI навигация
   $(".navigation-items").on("click",function()
   {
@@ -67,8 +89,8 @@ $(document).ready(function()
 		mainC.find('#deviceScreen').removeClass('hidden');
   }
   
-  // Функция за получаване на информацията
   
+  // Функция за получаване на информацията
   $(document).on("submit", ".registerForm", function()
   {
     event.preventDefault(); // Забраняваме изпълнението на form.submit(), защото пречи на 
@@ -94,10 +116,7 @@ $(document).ready(function()
 				}
 	var newUserID = Math.abs(hashCode(s));	
 	console.log(newUserID);
-	
-	
-	
-	
+		
 	// Setting data
 	user.set("userid", newUserID+"");
 	user.set("name", usernameForm);
@@ -120,75 +139,143 @@ $(document).ready(function()
 	  }
 	});
 	
-	
-
-	/*
-	// Save without checking for duplicate entries (version 1)
-	user.save(null, 
-	{
-	  success: function(object) 
-	  {
-		$(".success").show();
-		alert("Success!");
-		  
-	  },
-	  error: function(model, error) 
-	  {
-		$(".error").show();
-	  }
-	});
-	*/
-	
-	
-	// New version with check before duplicates
-	
-	/*
-	var GameScore = Parse.Object.extend("GameScore");
-
-	Parse.Cloud.beforeSave("GameScore", function(request, response) 
-	{
-		if (!request.object.isNew()) 
-		{
-		  // Let existing object updates go through
-		  response.success();
-		}
-		var query = new Parse.Query(GameScore);
-		// Add query filters to check for uniqueness
-		query.equalTo("playerName", request.object.get("playerName"));
-		query.first().then(function(existingObject) 
-		{
-		  if (existingObject) 
-		  {
-			// Update existing object
-			existingObject.set("score", request.object.get("score"));
-			return existingObject.save();
-		  } 
-		  else 
-		  {
-			// Pass a flag that this is not an existing object
-			return Parse.Promise.as(false);
-		  }
-		}).then(function(existingObject) 
-		  {
-			  if (existingObject) 
-			  {
-				// Existing object, stop initial save
-				response.error("Existing object");
-			  } 
-			  else 
-			  {
-				// New object, let the save go through
-				response.success();
-			  }
-		  }, function (error) {
-		  response.error("Error performing checks or saves.");
-		});
-	});
-	
-	*/
-	
-	
-	
   });
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  $(document).on("click", "#registerButtonFacebook", function()
+  {
+	  
+	  
+	  /*
+	function updateStatusCallback(){
+	alert('Status updated!!');
+		// Your logic here
+	}
+	  
+	$.ajaxSetup({ cache: true });
+	$.getScript('http://connect.facebook.net/en_US/sdk.js', function(){
+		FB.init({
+		  appId: '844853188931475',
+		  version: 'v2.3' // or v2.0, v2.1, v2.0
+		});     
+		//$('#loginbutton,#feedbutton').removeAttr('disabled');
+		FB.getLoginStatus(updateStatusCallback);
+	});
+	  
+	 
+	// This function is called when someone finishes with the Login
+	// Button.  See the onlogin handler attached to it in the sample
+	// code below.
+	function checkLoginState() {
+		FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+		});
+	}
+	
+ /*
+	window.fbAsyncInit = function() {
+	FB.init({
+    appId      : '844853188931475',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.3' // use version 2.2
+	});
+
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+
+  /*
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "http://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+  */	
+  
+  Parse.FacebookUtils.logIn(null, {
+  success: function(user) {
+    if (!user.existed()) {
+      alert("User signed up and logged in through Facebook!");
+    } else {
+      alert("User logged in through Facebook!");
+    }
+  },
+  error: function(user, error) {
+    alert("User cancelled the Facebook login or did not fully authorize.");
+  }
+});
+
+  
+});
+
+
+// Спомагателна функция за статус на логването във Facebook
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+}
+
+
+
+
 });

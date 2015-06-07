48dc3c7180bc091361b1bcea26c55e892534eb33
krisstamov91 <krisstamov91@gmail.com>
2015-06-07 14:01:25 +0300

    Update manualoverwrite.php

--- ManualOverride/manualoverwrite.php
+++ ManualOverride/manualoverwrite.php
@@ -6,17 +6,28 @@ $password = "";
 $dbname = "tuts_rest";
 if($_SERVER['REQUEST_METHOD'] == "POST"){
  // Get data
-  if(isset($_POST['user']) && isset($_POST['sleeptime']) && isset($_POST['turnaircond'])) {
+ if(isset($_POST['user'])) {
 //	$data=json_encode($_POST);
 $jsonString = file_get_contents("data.json");
+
 $conf = json_decode($jsonString);
-$conf->user=$_POST['user'];
-$conf->sleeptime=$_POST['sleeptime'];
-$conf->turnaircond=$_POST['turnircond'];
-$data = $conf;
 
 
+if(isset($_POST['sleeptime'])){
+	$conf->sleeptime=$_POST['sleeptime'];}
+if(isset($_POST['turnaircond'])){
+	$conf->turnaircond=$_POST['turnaircond'];}
+if(isset($_POST['turnlight'])){
+	$conf->turnlight=$_POST['turnlight'];}
+if(isset($_POST['temperature'])){
+	$conf->temperature=$_POST['temperature'];}
+if(isset($_POST['lightlevel'])){
+	$conf->lightlevel=$_POST['lightlevel'];}
+	
 
+
+	
+$data = json_encode($conf);
 //print_r($data);
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -24,12 +35,9 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 $user=$_POST['user'];
-
 $sql = "SELECT verified FROM users WHERE userid=$user";
 $result = mysqli_query($conn, $sql);
-
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
@@ -37,16 +45,14 @@ if (mysqli_num_rows($result) > 0) {
 		if($row["verified"]==1)
 			$ret = file_put_contents('data.json',$data);
 		else {
-			echo "No Permition To Execute Operation!";
+			echo "No Permision To Execute Operation!";
 		}
     }
 } else {
-    echo "No Permition To Execute Operation!";
+    echo "No Permision To Execute Operation!";
 }
-
 mysqli_close($conn);
  
 }
-
  
  }

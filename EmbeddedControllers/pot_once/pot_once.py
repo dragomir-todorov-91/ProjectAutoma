80db5aa916728b2d195d83ab462a6dd3ef50d842
Max <test@abv.bg>
2015-06-07 09:38:06 +0000

    Both controllers work together.

--- EmbeddedControllers/pot_once/pot_once.py
+++ EmbeddedControllers/pot_once/pot_once.py
@@ -2,16 +2,46 @@
 from __future__ import print_function
 import botbook_mcp3002 as mcp	# <1>
 import time
+import os #
+from test import Output
+
+def writeSensor(fileH):
+	x = str(mcp.readAnalog(0,0))	# <2>
+	ts = str(time.time())
+	print(x+","+ts, file=fileH)
+	print (x+","+ts)
+
+def writeTemperature(fileH):
+        x = str(mcp.readAnalog(0,0))    # <2>
+        ts = str(time.time())
+        print(x+","+ts, file=fileH)
+        print (x+","+ts)
+
+
 
 f = open('lightMeasurements.csv','a')
 
+pinH = Output()
 
-sleepfor = 15
+#open pin 4
 
 while True:
-	x = str(mcp.readAnalog(0,0))	# <2>
-	ts = str(time.time())
-	print(x+","+ts, file=f)
-	time.sleep(sleepfor)
-
+	pinH.open(4,"out")
+	#wait for pin to go high
+	time.sleep(1)
+	#measure light
+	writeSensor(f)
+	print("test")		
+	pinH.close(4)
+	#sleep for a second to avoid high voltage on pin 4
+	time.sleep(3)
+	pinH.open(17,"out")
+	#wait for pin to go high
+        time.sleep(1)
+        #measure temperature
+	writeTemperature(f)
+        print("test")
+        pinH.close(17)
+	#wait for the amaount of time set in configuration
+        time.sleep(3)
 
